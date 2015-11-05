9877e5b8ca72199d636e2274db2c1f9d9ce659ab
Daniel Fiala <freelancer@meave.biz>
2015-11-05 20:02:09 +0100

    Added calculation of time with clock_gettime() .

--- lib/gettime.hpp
+++ lib/gettime.hpp
@@ -3,6 +3,7 @@
 
 #include <cstdlib>
 #include <sys/time.h>
+#include <time.h>
 
 namespace meave {
 
@@ -11,6 +12,13 @@ namespace {
 double gettime() noexcept {
 	struct timeval tv;
 	if (-1 == gettimeofday(&tv, nullptr))
+		::abort(); // This could only happen by some mistake in the program.
+	return tv.tv_sec + tv.tv_usec/1000000.0;
+}
+
+double getrealtime() noexcept {
+	struct timeval tv;
+	if (-1 == clock_gettime(CLOCK_REALTIME, &tv))
 		::abort(); // This could only happen by some mistake in the program.
 	return tv.tv_sec + tv.tv_usec/1000000.0;
 }
