0dce84d93f6361697c3e97b142231bca9a29e830
Daniel Fiala <freelancer@meave.biz>
2015-11-10 22:05:51 +0100

    Fixed some mistakes.

--- crc/compare_speed.cpp
+++ crc/compare_speed.cpp
@@ -1,5 +1,13 @@
-#include <memory>
+#include <boost/crc.hpp>
 
+#include <algorithm>
+#include <iostream>
+#include <memory>
+#include <random>
+
+extern "C" {
+#include "crc/funcs.h"
+}
 
 /**
  * Compares speed and return value of various functions for calculation of CRC.
@@ -15,8 +23,8 @@ namespace {
 	struct CRC {
 		static const ::uint32_t POLY = 0x1EDC6F41;
 		static const ::size_t BITS = 32;
-		static ::uint32_t INIT_REM = 0;
-		static ::uint32_t FINAL_XOR = 0;
+		static const ::uint32_t INIT_REM = 0;
+		static const ::uint32_t FINAL_XOR = 0;
 		static const bool REFLECT_INPUT = false;
 		static const bool REFLECT_REMAINDER = false;
 
@@ -30,35 +38,33 @@ namespace {
 
 	::uint32_t calc_with_boost(const ::uint8_t *arr, const ::size_t len) {
 		CRC::Calc calc;
-		calc.process_bytes(&arr[0], &arr[len]);
+		calc.process_bytes(&arr[0], len);
 		return calc.checksum();
 	}
 
-	bool compare_output(const char *name, CRCCalf f) {
-		auto arr = $::make_unique<::uint8_t[]>(TestParams::LEN);
-		$::generate(&arr[0], &arr[TestParams::LEN], dist(rand));
+	bool compare_output(const char *name, CRCCalc f) {
+		auto arr = $::make_unique<::uint8_t[]>(LEN);
+		$::generate(&arr[0], &arr[LEN], dist(rand));
 
-		const auto expexted_res = calc_with_boost(arr, LEN);
-		const auto real_res = f(arr, LEN);
+		const auto expected_res = calc_with_boost(&arr[0], LEN);
+		const auto real_res = f(&arr[0], LEN);
 
-		const char is_passed = expexted_res == real_res;
+		const char is_passed = expected_res == real_res;
 		const char *verdict = is_passed ? "Passed" : "Failed";
 		$::cerr << "Test: " << name << "; expected: " << expexted_res << "; real: " << real_res << "; verdict: " << verdict << $::endl;
 
 		return is_passed;
 	}
 
-	void measure_speed(const char *name, CRCCalf f) {
-		auto arr = $::make_unique<::uint8_t[]>(TestParams::LEN);
-		$::generate(&arr[0], &arr[TestParams::LEN], dist(rand));
+	void measure_speed(const char *name, CRCCalc f) {
+		auto arr = $::make_unique<::uint8_t[]>(LEN);
+		$::generate(&arr[0], &arr[LEN], dist(rand));
 
 		const auto time_beg = getrealtime();
-		f(arr, LEN);
+		f(&arr[0], LEN);
 		const auto time_end = getrealtime();
 
-		const char is_passed = expexted_res == real_res;
-		const char *verdict = is_passed ? "Passed" : "Failed";
-		$::cerr << "Test: " << name << "; time: " << (time_end - time_beg) << "s" << $::endl;
+		$::cerr << "Speed: " << name << "; time: " << (time_end - time_beg) << "s" << $::endl;
 	}
 
 } /* Anonymouse Namespace */
---
+++ crc/funcs.h
@@ -0,0 +1,6 @@
+#ifndef MEAVE_CRC_FUNCS_H
+#	define MEAVE_CRC_FUNCS_H
+
+unsigned crc32_intel_asm(unsigned char *arr, unsigned len, unsigned val);
+
+#endif // MEAVE_CRC_FUNCS_H
