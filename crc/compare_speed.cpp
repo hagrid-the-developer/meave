cd3ce79162c15f3e95bbf924dbcb1e461d5d97eb
Daniel Fiala <freelancer@meave.biz>
2015-11-15 10:03:47 +0100

    CRC: Fixed a mistake in ASM code.
    	* Test still doesn't pass.

--- crc/compare_speed.cpp
+++ crc/compare_speed.cpp
@@ -34,6 +34,7 @@ namespace {
 	};
 
 	static const ::size_t LEN = 128*1024*1024;
+	static const ::size_t MAX_SUFF_LEN = 16;
 
 	RandomGenerator rand;
 	$::uniform_int_distribution<::uint8_t> dist;
@@ -45,36 +46,48 @@ namespace {
 	}
 
 	bool compare_output(const char *name, CRCCalc f) {
-		auto arr = $::make_unique<::uint8_t[]>(LEN);
-		$::generate(&arr[0], &arr[LEN], []() -> ::uint8_t { dist(rand); });
+		auto arr = $::make_unique<::uint8_t[]>(LEN + MAX_SUFF_LEN);
+		$::generate(&arr[0], &arr[LEN + MAX_SUFF_LEN], []() -> ::uint8_t { dist(rand); });
 
-		const auto expected_res = calc_with_boost(&arr[0], LEN);
-		const auto real_res = f(&arr[0], LEN, CRC::INIT_REM);
+		bool is_passed_all = false;
+		for (::size_t suff = 0; suff < MAX_SUFF_LEN; ++suff) {
+			const ::size_t len = LEN + suff;
+			const auto expected_res = calc_with_boost(&arr[0], len);
+			const auto real_res = f(&arr[0], len, CRC::INIT_REM);
 
-		const char is_passed = expected_res == real_res;
-		const char *verdict = is_passed ? "Passed" : "Failed";
-		$::cerr << "Test: " << name << "; expected: " << expected_res << "; real: " << real_res << "; verdict: " << verdict << $::endl;
+			const char is_passed = expected_res == real_res;
+			const char *verdict = is_passed ? "Passed" : "Failed";
+			$::cerr << "Test: " << name << "; suff_len: " << suff << "; expected: " << expected_res << "; real: " << real_res << "; verdict: " << verdict << $::endl;
 
-		return is_passed;
+			is_passed_all = is_passed_all && is_passed;
+		}
+
+		return is_passed_all;
 	}
 
 	void measure_speed(const char *name, CRCCalc f) {
-		auto arr = $::make_unique<::uint8_t[]>(LEN);
-		$::generate(&arr[0], &arr[LEN], []() -> ::uint8_t { return dist(rand); });
+		auto arr = $::make_unique<::uint8_t[]>(LEN + MAX_SUFF_LEN);
+		$::generate(&arr[0], &arr[LEN + MAX_SUFF_LEN], []() -> ::uint8_t { return dist(rand); });
 
-		const auto time_beg = meave::getrealtime();
-		f(&arr[0], LEN, CRC::INIT_REM);
-		const auto time_end = meave::getrealtime();
+		for (::size_t suff = 0; suff < MAX_SUFF_LEN; ++suff) {
+			const ::size_t len = LEN + suff;
+			const auto time_beg = meave::getrealtime();
+			f(&arr[0], len, CRC::INIT_REM);
+			const auto time_end = meave::getrealtime();
 
-		$::cerr << "Speed: " << name << "; time: " << (time_end - time_beg) << "s" << $::endl;
+			$::cerr << "Speed: " << name << "; suff_len: " << suff << "; time: " << (time_end - time_beg) << "s" << $::endl;
+		}
 	}
 
 } /* Anonymouse Namespace */
 
 int main(void) {
 	compare_output("crc32_intel_asm", crc32_intel_asm);
+	$::cerr << $::endl;
+	$::cerr << $::endl;
 	/* */
 	measure_speed("calc_with_boost", calc_with_boost);
+	$::cerr << $::endl;
 	measure_speed("crc32_intel_asm", crc32_intel_asm);
 	/* */
 	return 0;
