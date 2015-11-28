60b9e6b85196c2a74bfcacbaf0e187d276b5dded
Daniel Fiala <freelancer@meave.biz>
2015-11-28 21:32:47 +0100

    Merge-sort uses std::move instead of copy.

--- algorithms/merge-sort.hpp
+++ algorithms/merge-sort.hpp
@@ -2,7 +2,7 @@
 #   define MEAVE_ALGORITHMS_MERGE_SORT_HPP
 
 #   include <algorithm>
-#   include <boost/assert.hpp>
+#   include <cassert>
 
 #   include <meave/commons.hpp>
 
@@ -14,70 +14,72 @@ namespace meave { namespace algorithms {
     template <typename It, typename Lt>
     class MergeSort : protected Lt {
     public:
-        typedef $::iterator_traits<It> V;
+        typedef $::iterator_traits<It> Traits;
+	typedef typename V::difference_type Distance;
+	typedef typename Distance D;
 
     private:
         It b_, q_;
-        $::size_t len_;
+        D len_;
 
     public:
-        MergeSort(const It &b, const It &q, const $::size_t len, const Lt &lt)
+        MergeSort(const It &b, const It &q, const D len, const Lt &lt)
         :   Lt(lt),
             b_(b),
             q_(q),
             len_(len)
         {
-            BOOST_VERIFY($::numeric_limits<$::size_t>::max()/2 >= len_);
+            assert($::numeric_limits<D>::max()/2 >= len_);
         }
 
         /**
          *
          */
-        It merge_(It ot, It it, const $::size_t from, const size_t step) const {
-            const $::size_t mid = from + step;
-            const $::size_t end = mid + step;
-            $::size_t i = from;
-            $::size_t j = mid;
+        It merge_(It ot, It it, const D from, const D step) const {
+            const D mid = from + step;
+            const D end = mid + step;
+            D i = from;
+            D j = mid;
             for (; i < $::min(mid, len_) && j < $::min(end, len_); ++ot) {
                 if ((*this)(it[j], it[i])) {
-                    *ot = it[j++];
+                    *ot = $::move(it[j++]);
                 } else {
-                    *ot = it[i++];
+                    *ot = $::move(it[i++]);
                 }
             }
             for (; i < $::min(mid, len_); ++ot) {
-                *ot = it[i++];
+                *ot = $::move(it[i++]);
             }
             for (; j < $::min(end, len_); ++ot) {
-                *ot = it[j++];
+                *ot = $::move(it[j++]);
             }
             return ot;
         }
  
-        $::size_t step_(const $::size_t step) const {
+        D step_(const D step) const {
             return 1 << step;
         }
 
         void operator()(void) {
             It b = b_;
             It q = q_;
-            $::size_t $ = 0;
+            D $ = 0;
             for (; step_($) < len_; ++$) {
                 It o = q;
-                for ($::size_t i = 0; i < len_; i += 2*step_($)) {
+                for (D i = 0; i < len_; i += 2*step_($)) {
                     o = merge_(o, b, i, step_($));
                 }
                 $::swap(b, q);
             }
             if (1 == $ % 2) {
                 BOOST_ASSERT(b == q_ && q == b_);
-                $::copy(q_, q_ + len_, b_);
+                $::move(q_, q_ + len_, b_);
             }
         }
     };
 
     template<typename It, typename Lt>
-    void merge_sort(const It &b, const It &q, const $::size_t len, const Lt &lt) {
+    void merge_sort(const It &b, const It &q, const D len, const Lt &lt) {
         MergeSort<It, Lt>(b, q, len, lt)();
     }
 } }
