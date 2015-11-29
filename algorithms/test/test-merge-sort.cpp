c07c0cdfd720424791fb2f1080b85a87a54909d8
Daniel Fiala <freelancer@meave.biz>
2015-11-29 15:26:10 +0100

    Added test for object based on int.

--- algorithms/test/test-merge-sort.cpp
+++ algorithms/test/test-merge-sort.cpp
@@ -14,14 +14,17 @@
 
 using namespace boost::assign;
 
-static bool x001(void) {
-    $::vector<int> arr, crr;
+namespace {
+
+template<typename T>
+bool x001(void) {
+    $::vector<T> arr, crr;
     arr += 1000, 200, 30, 4;
     crr = arr;
 
-    $::vector<int> brr(arr.size());
+    $::vector<T> brr(arr.size());
 
-    const auto cmp = [](const int x, const int y) -> bool { return x < y; };
+    const auto cmp = [](const T &x, const T &y) -> bool { return x < y; };
     meave::algorithms::merge_sort(arr.begin(), brr.begin(), arr.size(), cmp);
 
     for ($::size_t i = 0; i < arr.size(); ++i) {
@@ -33,8 +36,9 @@ static bool x001(void) {
     return $::equal(arr.begin(), arr.end(), crr.begin());
 }
 
-static bool x002(const unsigned len) {
-    $::vector<int> arr, crr, brr(len);
+template<typename T>
+bool x002(const unsigned len) {
+    $::vector<T> arr, crr, brr(len);
     for (unsigned i = 0; i < len; ++i) {
         arr.push_back(rand() % 22222);
     }
@@ -42,7 +46,7 @@ static bool x002(const unsigned len) {
     crr = arr;
     BOOST_VERIFY($::equal(arr.begin(), arr.end(), crr.begin()));
 
-    const auto cmp = [](const int x, const int y) -> bool { return x < y; };
+    const auto cmp = [](const T &x, const T &y) -> bool { return x < y; };
     meave::algorithms::merge_sort(arr.begin(), brr.begin(), arr.size(), cmp);
     $::sort(crr.begin(), crr.end(), cmp);
 
@@ -54,15 +58,15 @@ static bool x002(const unsigned len) {
     return $::equal(arr.begin(), arr.end(), crr.begin());
 }
 
-int
-main(void) {
+template<typename T>
+bool test(const $::string &test_name) {
     typedef $::pair<$::string, boost::function< bool(void) > > TestPair;
-    $::vector<TestPair> tests = list_of<TestPair>("x001", &x001);
+    $::vector<TestPair> tests = list_of<TestPair>("x001", &x001<T>);
     for (unsigned i = 3; i <= 100; i += rand() % 5) {
-        tests.push_back($::make_pair(meave::str_printf("x002(%u)", i), [i]() -> bool {return x002(i); }));
+        tests.push_back($::make_pair(meave::str_printf("x002(%u)", i), [i]() -> bool {return x002<T>(i); }));
     }
     for (unsigned i = 101; i <= 200000; i += rand() % 10000) {
-        tests.push_back($::make_pair(meave::str_printf("x002(%u)", i), [i]() -> bool {return x002(i); }));
+        tests.push_back($::make_pair(meave::str_printf("x002(%u)", i), [i]() -> bool {return x002<T>(i); }));
     }
     unsigned failures_num = 0;
     for (auto it(tests.begin()), e(tests.end()); it != e; ++it) {
@@ -76,7 +80,135 @@ main(void) {
         }
     }
 
-    $::cerr << "Number of tests: " << tests.size() << "; failed: " << failures_num << $::endl;
+    $::cerr << "Test: " << test_name << "; Number of tests: " << tests.size() << "; failed: " << failures_num << $::endl;
 
-    return failures_num == 0 ? 0 : 1;
+    return 0 == failures_num;
+}
+
+class Int {
+protected:
+	int _;
+
+public:
+	Int()
+	:	_(0) {
+	}
+	Int(const int $)
+	:	_($) {
+	}
+	Int(const Int&) = default;
+	Int(Int&&) = default;
+
+	Int& operator=(const Int&) = default;
+	Int &operator=(Int&&) = default;
+
+	int operator*() const {
+		return _;
+	}
+	bool operator<(const Int &that) const {
+		return _ < *that;
+	}
+	bool operator==(const Int &that) const {
+		return _ == *that;
+	}
+	bool operator>(const Int &that) const {
+		return _ > *that;
+	}
+};
+
+$::ostream &operator<<($::ostream &o, const Int &$) {
+	return o << *$;
+}
+
+class IntWithoutMove : public Int {
+public:
+	IntWithoutMove()
+	:	Int() {
+	}
+	IntWithoutMove(const int $)
+	:	Int($) {
+	}
+	IntWithoutMove(const IntWithoutMove&) = default;
+	IntWithoutMove(IntWithoutMove&&) = delete;
+
+	IntWithoutMove& operator=(const IntWithoutMove&) = default;
+	IntWithoutMove& operator=(IntWithoutMove&&) = delete;
+};
+
+class IntWithCountedCopyMove : public Int {
+private:
+	static unsigned copy_num;
+	static unsigned move_num;
+
+public:
+	static unsigned copy() {
+		return copy_num;
+	}
+	static unsigned move() {
+		return move_num;
+	}
+
+	IntWithCountedCopyMove()
+	:	Int() {
+	}
+	IntWithCountedCopyMove(const int $)
+	:	Int($) {
+	}
+	IntWithCountedCopyMove(const IntWithCountedCopyMove& that)
+	:	Int(that) {
+		++copy_num;
+	}
+	IntWithCountedCopyMove(IntWithCountedCopyMove&& that)
+	:	Int(that){
+		++move_num;
+	}
+
+	IntWithCountedCopyMove& operator=(const IntWithCountedCopyMove &that) {
+		++copy_num;
+		_ = *that;
+
+		return *this;
+	}
+	IntWithCountedCopyMove& operator=(IntWithCountedCopyMove &&that) {
+		++move_num;
+		_ = *that;
+
+		return *this;
+	}
+};
+
+unsigned IntWithCountedCopyMove::copy_num = 0;
+unsigned IntWithCountedCopyMove::move_num = 0;
+
+} /* anonymous namespace */
+
+int
+main(void) {
+	// Test without move is disabled, because evan $::move() cannot hold it.
+
+	const bool test_int_succ = test<int>("int");
+//	const bool test_without_move = test<IntWithoutMove>("Int without move");
+	const bool test_counted_copy_move = test<IntWithCountedCopyMove>("Int counted copy and move");
+
+//	const unsigned tests_passed = test_int_succ + test_without_move + test_counted_copy_move;
+	const unsigned tests_passed = test_int_succ + test_counted_copy_move;
+	const unsigned tests_failed = 2 - tests_passed;
+
+	$::cerr << "Test with int: "
+		<< (test_int_succ ? "Passed" : "Failed")
+		<< $::endl;
+//	$::cerr << "Test without move: "
+//		<< (test_without_move ? "Passed" : "Failed")
+//		<< $::endl;
+	$::cerr << "Test with counted copy and move: "
+		<< (test_counted_copy_move ? "Passed" : "Failed")
+		<< "; copies: " << IntWithCountedCopyMove::copy()
+		<< "; moves: " << IntWithCountedCopyMove::move()
+		<< $::endl;
+	$::cerr << $::endl;
+	$::cerr << "Tests passed: "
+		<< tests_passed << "; tests failed: " << tests_failed
+		<< $::endl;
+
+	return tests_failed > 0 ? 1 : 0;
 }
