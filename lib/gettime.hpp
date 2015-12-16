80c5cd5a2b7b042532418efab8564db11ce80b22
Daniel Fiala <freelancer@meave.biz>
2015-12-16 22:18:17 +0100

    Forgotten header in gettime.hpp .

--- lib/gettime.hpp
+++ lib/gettime.hpp
@@ -2,6 +2,7 @@
 #define MEAVE_GETTIME_HPP_INCLUDED
 
 #include <cstdlib>
+#include <ctime>
 #include <sys/time.h>
 #include <time.h>
 
@@ -11,14 +12,14 @@ namespace {
 
 double gettime() noexcept {
 	struct timeval tv;
-	if (-1 == gettimeofday(&tv, nullptr))
+	if (-1 == ::gettimeofday(&tv, nullptr))
 		::abort(); // This could only happen by some mistake in the program.
 	return tv.tv_sec + tv.tv_usec/1000000.0;
 }
 
 double getrealtime() noexcept {
 	struct timespec ts;
-	if (-1 == clock_gettime(CLOCK_REALTIME, &ts))
+	if (-1 == ::clock_gettime(CLOCK_REALTIME, &ts))
 		::abort(); // This could only happen by some mistake in the program.
 	return ts.tv_sec + ts.tv_nsec/1000000000.0;
 }
