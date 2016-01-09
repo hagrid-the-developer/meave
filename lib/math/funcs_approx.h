677300cbc9ed2334133d61a3cb78142f9833cc75
Daniel Fiala <freelancer@meave.biz>
2016-01-09 13:57:55 +0100

    Other implementation of exp approximation.

--- lib/math/funcs_approx.h
+++ lib/math/funcs_approx.h
@@ -1,7 +1,6 @@
 #ifndef MEAVE_MATH_FUNCS_APPROX_H_INCLUDED
 #	define MEAVE_MATH_FUNCS_APPROX_H_INCLUDED
 
-
 exp_pade22(float *dst, float *src, unsigned len);
 
 #endif // MEAVE_MATH_FUNCS_APPROX_H_INCLUDED
