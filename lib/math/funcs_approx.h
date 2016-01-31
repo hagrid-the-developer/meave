3ec6a1024125c083c9b09ef4e2b769ade413ed47
Daniel Fiala <freelancer@meave.biz>
2016-01-31 19:26:03 +0100

    Added test related to pade22, not falling, values wrong.

--- lib/math/funcs_approx.h
+++ lib/math/funcs_approx.h
@@ -1,6 +1,6 @@
 #ifndef MEAVE_MATH_FUNCS_APPROX_H_INCLUDED
 #	define MEAVE_MATH_FUNCS_APPROX_H_INCLUDED
 
-exp_pade22(float *dst, float *src, unsigned len);
+void exp_pade22(float *dst, const float *src, const unsigned len);
 
 #endif // MEAVE_MATH_FUNCS_APPROX_H_INCLUDED
---
+++ lib/raii/mmap_pointer.hpp
@@ -0,0 +1,57 @@
+#ifndef MEAVE_RAII_MMAP_POINTER_HPP_INCLUDED
+#	define MEAVE_RAII_MMAP_POINTER_HPP_INCLUDED
+
+#include <cstdlib>
+#include <meave/lib/error.hpp>
+#include <meave/lib/raii/mmap_mem.hpp>
+
+namespace meave { namespace raii {
+
+template<typename T>
+class MMapPointer {
+private:
+	MMapMem p_;
+
+public:
+	MMapPointer() noexcept
+	:	p_(0)
+	{ }
+	MMapPointer(const ::size_t len, const int mmap_flags = 0) throw(Error)
+	:	p_(len * sizeof(T), mmap_flags)
+	{ }
+	MMapPointer(const MMapPointer &) = delete;
+	MMapPointer(MMapPointer &&x) = default;
+
+	MMapPointer &operator=(const MMapPointer &) = delete;
+	MMapPointer &operator=(MMapPointer &&x) = default;
+
+	T *operator*() noexcept {
+		return reinterpret_cast<T*>(*p_);
+	}
+
+	const T *operator*() const noexcept {
+		return reinterpret_cast<const T*>(*p_);
+	}
+
+	T &operator[](const ::size_t index) noexcept {
+		return (**this)[index];
+	}
+
+	const T &operator[](const ::size_t index) const noexcept {
+		return (**this)[index];
+	}
+
+	::size_t size() const noexcept {
+		return p_.size();
+	}
+
+	::size_t len() const noexcept {
+		return size()/sizeof(T);
+	}
+
+	~MMapPointer() = default;
+};
+
+} } /* namespace meave::raii */
+
+#endif // MEAVE_RAII_MMAP_POINTER_HPP_INCLUDED
