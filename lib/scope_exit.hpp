8282ea291cbc5d02a93a3be999cb0c341db9e49c
Daniel Fiala <freelancer@meave.biz>
2016-03-21 08:26:25 +0100

    Removed unused libraries, destructor is noexcept.

--- lib/scope_exit.hpp
+++ lib/scope_exit.hpp
@@ -1,11 +1,6 @@
 #ifndef MEAVE_SCOPE_EXIT_HPP_INCLUDED
 #define MEAVE_SCOPE_EXIT_HPP_INCLUDED
 
-#include <cstdlib>
-#include <ctime>
-#include <sys/time.h>
-#include <time.h>
-
 namespace meave {
 
 namespace {
@@ -15,7 +10,7 @@ struct ScopeExit {
 	F f_;
 
 	ScopeExit(F f) : f_(f) {}
-	~ScopeExit() { f_(); }
+	~ScopeExit() noexcept { f_(); }
 };
 
 template <typename F>
