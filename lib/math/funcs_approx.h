a9af7a27c88a2a96281274b74eb7c94fe4718b94
Daniel Fiala <freelancer@meave.biz>
2016-10-18 04:58:21 +0200

    Added other tests and exp2 function for one v8sf value only.

--- lib/math/funcs_approx.h
+++ lib/math/funcs_approx.h
@@ -4,4 +4,12 @@
 void exp_pade22(float *dst, const float *src, const unsigned len);
 void exp2_taylor(float *dst, const float *src, const unsigned len);
 
+
+typedef float v8sf __attribute__((vector_size(32)));
+
+v8sf exp2_taylor_one(const v8sf $);
+
+// Functions from avx2_math
+v8sf exp256_ps(v8sf);
+
 #endif // MEAVE_MATH_FUNCS_APPROX_H_INCLUDED
