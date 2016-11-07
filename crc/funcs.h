e1ae2654e312ce83af77661ffecd087aacc32cbb
Daniel Fiala <freelancer@meave.biz>
2016-11-07 03:47:16 +0100

    Implementation of CRC ASM function with merging.

--- crc/funcs.h
+++ crc/funcs.h
@@ -3,7 +3,8 @@
 
 #include <stdint.h>
 
-unsigned crc32_intel_asm(const unsigned char *arr, const unsigned long len, const unsigned val);
+unsigned crc32_intel_asm(const unsigned char *arr, const unsigned long len, const unsigned val) __attribute__((pure));
+unsigned crc32_threesome_kernel(const unsigned char *arr, const unsigned long len, const unsigned val) __attribute__((pure));
 uint32_t sse42_crc32(const uint8_t *bytes, const size_t len);
 
 #endif // MEAVE_CRC_FUNCS_H
