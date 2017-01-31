0b3dfd5865adf51201adeb0e2a2e2624790233b1
Daniel Fiala <freelancer@meave.biz>
2017-01-31 08:23:55 +0100

    Fixed mistake in SSE union, added b_[?] .

--- lib/rothash/rothash_avx2.hpp
+++ lib/rothash/rothash_avx2.hpp
@@ -10,8 +10,9 @@ union SSE {
 	__m128i i4_;
 	__m128 f4_;
 	__m128d d2_;
-	float sf_[8];
-	::uint32_t dw_[8];
+	float sf_[4];
+	::uint32_t dw_[4];
+	::uint8_t b_[16];
 };
 
 union AVX {
@@ -23,6 +24,7 @@ union AVX {
 	SSE sse_[2];
 	::uint32_t dw_[8];
 	::uint64_t qw_[4];
+	::uint8_t b_[32];
 };
 
 } } /* meave::vec */
