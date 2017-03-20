efd68eb0812802b5b07af43d783c82a6f1be611b
Daniel Fiala <freelancer@meave.biz>
2017-03-20 04:41:07 +0100

    Removed unused part of the code.

--- lib/math/precalculate_log.hpp
+++ lib/math/precalculate_log.hpp
@@ -8,47 +8,6 @@ namespace meave { namespace math {
 
 namespace aux {
 
-#if 0
-template <float F(float), ::ssize_t SAMPLES, ::ssize_t MIN, ::ssize_t MAX, ::ssize_t DIVISOR>
-class Precalculate {
-private:
-	float data_[SAMPLES + 1];
-
-	constexpr float min_f() const noexcept {
-		return float(MIN)/DIVISOR;
-	}
-	constexpr float max_f() const noexcept {
-		return float(MAX)/DIVISOR;
-	}
-
-	constexpr float val(const ::size_t i) const noexcept {
-		return (max_f() - min_f())*i/SAMPLES + m;
-	}
-
-	meave::vec::AVX min() const noexcept {
-		return meave::vec::AVX{ .f8_ = _mm256_set1_ps(val(0)) };
-	}
-
-	meave::vec::AVX max() const noexcept {
-		return meave::vec::AVX{ .f8_ = _mm256_set1_ps(val(SAMPLES)) };
-	}
-
-public:
-	Precalculate() noexcept {
-		for (::ssize_t i = 0; i < SAMPLES + 1; ++i) {
-			data_[i] = F(val(i));
-		}
-	}
-
-	meave::vec::AVX operator()(meave::vec::AVX $) noexcept {
-		$.f8_ = _mm256_min_ps(max().f8_, _mm256_max_ps(min().f8_, $.f8_));
-		meave::vec::AVX index_f{ .f8_ = _mm256_set1_ps( ($.f8_*SAMPLES - min())/(max() - min()) }; // !!Division, so this will be slow!
-		meave::vec::AVX index{ .i8_ = _mm256_cvtps_epi32(index_f.f8_) };
-		return meave::vec::AVX{ .f8_ = _mm256_i32gather_ps(data_, index.i8_, 4) };
-	}
-};
-#endif
-
 template <float F(float), unsigned SAMPLES = 1000>
 class Precalculate {
 private:
